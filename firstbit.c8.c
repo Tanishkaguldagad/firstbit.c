b5243029bbc87500fbfe53e63871dd8c991c38a9
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2025-07-26 02:03:51 +0530

    Write a C program to input the length and width of a rectangle and find its perimeter.

---
+++ firstbit.c8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int length=8;
+ int breadth=6;
+ int area;
+ int perimeter;
+ area=length*breadth;
+ perimeter=2*(length+breadth);
+ printf("%d %d",area,perimeter);
+}
