2a647cf9b9bf18e983400e51eac7b7c9f7c8c2e7
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2025-08-17 13:20:32 +0530

    Print armstrong numbers in the given range 1 to n.

---
+++ assignment4.q1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	for(int k=1;k<=1000;k++)
+	{
+     int no=k;
+	int temp=no;
+	int count =0;
+	while(no>0)
+	{
+		count++;
+		no=no/10;
+		}
+		no=temp;
+		int sum=0,rem;
+		while(no>0)
+		{
+			rem=no%10;
+			int res=1;
+			for(int i=1;i<=count;i++)
+			res=res*rem;
+			sum=sum+res;
+			no=no/10;
+			}	
+			if(sum==temp)
+			printf("%d is armstrong\n",temp);
+			else
+			printf("%d is not armstrong\n",temp);
+	}
+}
